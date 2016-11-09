8c0d1af4c9638f818721fa1eb63843b45ab04c19
Eternity-Yarr <savraz@gmail.com>
2016-11-09 11:51:24 +0300

    reduce to minimal working example

--- src/main/java/jpa/TestApplication.java
+++ src/main/java/jpa/TestApplication.java
@@ -2,11 +2,31 @@ package jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @SpringBootApplication
+@Controller
 public class TestApplication {
+  @RequestMapping(
+    path = "/bug",
+    method = RequestMethod.POST,
+    consumes = MediaType.APPLICATION_JSON_UTF8_VALUE
+  )
+  public String test(List<String> entities) {
+    try {
+      entities.size();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return "i'm fine";
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TestApplication.class, args);
+  }
 }
